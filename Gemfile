a93b149b43c2507f7f1a582ab68d8f9e20027956
Felisa <felisa.kh@hotmail.com>
2019-04-08 16:17:31 +0000

    create buttn now works and updated kaminari pagination

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
+gem 'kaminari'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
 # Use postgresql as the database for Active Record
@@ -72,4 +74,4 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
-gem 'kaminari'
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,20 @@
 class PlacesController < ApplicationController
-
   def index
-    @places = Place.all.page(params[:page]).per(5)
+    @places = Place.page(params[:page]).per(5)
   end
 
   def new
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
