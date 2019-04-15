77822dde228e91f623b9c49a5af351cd5d308c28
Felisa <felisa.kh@hotmail.com>
2019-04-15 21:11:32 +0000

    almost done with the images

--- Gemfile
+++ Gemfile
@@ -78,6 +78,8 @@ gem 'geocoder'
 
 gem 'figaro'
 
+gem 'carrierwave', '~> 1.0'
+
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,6 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:caption, :rating)
+    params.require(:photo).permit(:caption)
   end
 end
