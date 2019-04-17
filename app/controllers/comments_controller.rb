3eee3457a85fc77eecfca78fd5f8e6ec52dc11e2
Felisa <felisa.kh@hotmail.com>
2019-04-17 21:12:27 +0000

    adding images with aws

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,5 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:message, :rating)
   end
+
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @photo = @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:picture, :caption)
   end
 end
