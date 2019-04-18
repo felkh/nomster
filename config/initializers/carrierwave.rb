ca277aabcdeb23a11e6577b843f6f0093dab939b
Felisa <felisa.kh@hotmail.com>
2019-04-18 17:44:39 +0000

    images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,4 +6,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
 end
