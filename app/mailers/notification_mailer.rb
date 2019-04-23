e0ef0c3430fed4f60080b19262fd8908fdbd7d25
Felisa <felisa.kh@hotmail.com>
2019-04-23 20:32:38 +0000

    email fixing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,8 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added(comment)
-    @place = comment.place
-    @place_owner = @place.user
-    mail(to: @place_owner.email,
+  def comment_added
+    mail(to: "fpkhalill1@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
-  after_create :send_comment_email
 
   RATINGS = {
     'one star': '1_star',
@@ -13,9 +12,5 @@ class Comment < ApplicationRecord
 
   def humanized_rating
     RATINGS.invert[self.rating]
-  end
-
-  def send_comment_email
-    NotificationMailer.comment_added(self).deliver_now
   end
 end
